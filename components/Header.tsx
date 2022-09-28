4464aaecc848d71c3de8fba956fd4427b4dc0295
James Bream <JamesBream@users.noreply.github.com>
2022-09-29 01:04:25 +0200

    feat: Header image priority and placeholder blur

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div className="flex select-none flex-row">
       <div className="w-28">
-        <Image src={logo} alt="I'm Sleepy Logo" />
+        <Image src={logo} alt="I'm Sleepy Logo" priority placeholder={"blur"} />
       </div>
       <div className="flex flex-1 flex-col items-center justify-center pl-4">
         <div className="bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-3xl font-light text-transparent">
