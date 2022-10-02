3f5cacf635f9e288948592d3788b1f226b0e6e2e
James Bream <JamesBream@users.noreply.github.com>
2022-10-02 23:48:02 +0200

    feat: responsive design

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,14 +4,16 @@ import logo from "../public/logo-sm.webp";
 export default function Header() {
   return (
     <div className="flex select-none flex-row">
-      <div className="w-28">
+      <div className="w-16 sm:w-28">
         <Image src={logo} alt="I'm Sleepy Logo" priority placeholder={"blur"} />
       </div>
       <div className="flex flex-1 flex-col items-center justify-center pl-4">
-        <div className="bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-3xl font-light text-transparent">
+        <div className="bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-2xl font-light text-transparent sm:text-3xl">
           I&apos;m Sleepy
         </div>
-        <div className="font-medium uppercase">Bedtime calculator</div>
+        <div className="text-sm font-medium uppercase sm:text-base">
+          Bedtime calculator
+        </div>
       </div>
     </div>
   );
