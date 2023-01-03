b88d1834f589fb5280bff52701098edfc0e8aa5b
James Bream <JamesBream@users.noreply.github.com>
2023-01-03 23:02:53 +0000

    feat: Smooth transitions on the Button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,11 @@ type Props = {
 
 function Button({ children }: Props) {
   return (
-    <button className="rounded-full border border-slate-100/20 from-indigo-500 via-purple-500 to-pink-500 py-2 px-4 no-underline transition-all hover:bg-gradient-to-br hover:transition-all">
+    <button className="relative overflow-hidden rounded-full border border-slate-100/20 py-2 px-4 no-underline">
+      {/* An ugly workaround to not being able to use CSS transitions on linear-gradients :( */}
+      <div className="absolute inset-0 rounded-full bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 pt-2 opacity-0 transition-opacity hover:opacity-100">
+        {children}
+      </div>
       {children}
     </button>
   );
